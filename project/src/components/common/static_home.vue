86742327162470fd6c90538d4d43997220a20dc3
gany <1049783847@qq.com>
2019-01-29 14:36:42 +0800

    aa

---
+++ project/src/components/common/static_home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="static_home">
+    我是static_home
+
+
+      <navs ></navs>
+
+    <sheader :ban="banner"></sheader>
+    <div class="content">
+      <transition name="move" mode="out-in">
+        <router-view/>
+      </transition>
+    </div>
+    <vfooter></vfooter>
+  </div>
+</template>
+
+<script>
+  import vfooter from './vfooter'
+  import sheader from './s_header.vue';
+  import navs from './navs.vue';
+
+  export default {
+    components: {
+      vfooter,
+      sheader,
+      navs
+    },
+    name: "static_home",
+    data() {
+      return {
+        isDisplay:true,
+        banner:this.$img.newsBan,
+        navList:[]
+      }
+    },
+    methods: {
+      handleSelect() {
+      },
+      monitorRouter(){
+      }
+    },
+    watch:{
+      '$route':'monitorRouter'
+    },
+    mounted() {
+      this.monitorRouter();
+    },
+  }
+</script>
+
+<style scoped>
+  @media only screen and (min-width: 500px){
+    .content {
+      min-height: 540px;
+      background-color: white;
+      padding: 0;
+    }
+  }
+</style>
