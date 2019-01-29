ccf0ff5d485c7af66efd1e3c6e7ff8fb5cd734b4
gany <1049783847@qq.com>
2019-01-29 15:28:25 +0800

    aa

--- project/src/components/common/navs.vue
+++ project/src/components/common/navs.vue
@@ -29,56 +29,71 @@
 </template>
 
 <script>
-    export default {
-      name: "navs",
-      data() {
-        return {
-          activeIndex2: 0,
-          listData: [],
-          oneDetail: [
-            {value: '公司简介', path: 'index'},
-            {value: '公司文化', path: 'film', id: 0},
-            {value: '组织架构', path: 'login'},
-            {value: '资质荣誉', path: 'about_culture', id: 1},
-            {value: '管理团队', path: 'join_us'},
-            {value: '团队风采', path: 'contact_us'},
-          ],
-          twoDetail: [
-            {value: '业务领域', path: 'index'},
-            {value: '项目成果', path: 'film', id: 0},
-            {value: '产品展示', path: 'login'},
-            {value: '解决方案', path: 'about_culture', id: 1},
-            {value: '技术支持', path: 'join_us'},
+  export default {
+    name: "navs",
+    data() {
+      return {
+        activeIndex2: 0,
+        listData: [],
+        oneDetail: [
+          {value: '公司简介', path: 'index'},
+          {value: '公司文化', path: 'film', id: 0},
+          {value: '组织架构', path: 'login'},
+          {value: '资质荣誉', path: 'about_culture', id: 1},
+          {value: '管理团队', path: 'join_us'},
+          {value: '团队风采', path: 'contact_us'},
+        ],
+        twoDetail: [
+          {value: '业务领域', path: 'index'},
+          {value: '项目成果', path: 'film', id: 0},
+          {value: '产品展示', path: 'login'},
+          {value: '解决方案', path: 'about_culture', id: 1},
+          {value: '技术支持', path: 'join_us'},
 
-          ],
-        }
-      },
-      methods: {
-
-
-
-
-
+        ],
       }
+    },
+    methods: {
+
+
+
+
+
     }
+  }
 </script>
 
 <style scoped>
-
-    .nav ul{
-        list-style: none;
-        width:1200px;
-        margin: 0 auto;
-    }
-    .nav li{
-        float:left;
-        margin-right: 60px;
-        color:#636363;
-        height:55px;
-        line-height: 55px;
-        cursor: pointer;
-        box-sizing: border-box;
-    }
-
-
+  .nav {
+    border-bottom: solid 1px #e6e6e6;
+    box-sizing: border-box;
+  }
+  .nav ul{
+    list-style: none;
+    width:1200px;
+    margin: 0 auto;
+  }
+  .nav li{
+    float:left;
+    margin-right: 60px;
+    color:#636363;
+    height:55px;
+    line-height: 55px;
+    cursor: pointer;
+    box-sizing: border-box;
+  }
+  .nav .active{
+    border-bottom:2px solid #32cd32 !important;
+  }
+  .sNav {
+    height: 101px;
+    width: 45%;
+    background: white;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 7.5px;
+    border-radius: 3px;
+  }
 </style>
